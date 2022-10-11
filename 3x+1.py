ba786cf3dc780ff4006d30ebd9b264da3276e2f6
Sterling-Schlink <112117696+Sterling-Schlink@users.noreply.github.com>
2022-10-11 00:36:28 -0700

    added iteration and highest number outputs
    i did this at like 12:30 in the morning because i suddenly got inspiration to do so

--- 3x+1.py
+++ 3x+1.py
@@ -1,4 +1,6 @@
 a = int(input("Enter a number: "))
+b = 0
+c = 0
 while 1 == 1:
     if a == 1:
         break
@@ -6,5 +8,10 @@ while 1 == 1:
         a = (a * 3) + 1
     elif (a % 2) == 0:
         a = a / 2
+    b = b + 1
+    if a >= c:
+        c = a
     print(str(a))
 print("Fail")
+print(str(b) + " iterations")
+print("Highest Number: " + str(c))
